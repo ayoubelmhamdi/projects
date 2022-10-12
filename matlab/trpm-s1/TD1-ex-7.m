6bfe01ff3222e81d1d0e3df14e08106c1ab428ca
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-10-12 21:58:38 +0100

    commit Wed Oct 12 09:58:38 PM CET 2022

---
+++ matlab/trpm-s1/TD1-ex-7.m
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+  int a, b, c, d, aide;
+  printf("Donner les valeurs de a et b \n");
+  scanf("%d%d", &a, &b);
+
+  // permutation pourque la valeur max=a et min=b
+  if (a > b) {
+    c = a;
+    d = b;
+  } else {
+    c = b;
+    d = a;
+  }
+
+  // PGCD
+
+  int rest = 1;
+  while (rest != 0) {
+    rest = c % d;
+    c = d;
+    d = rest;
+  }
+  printf("le PGCD de %d et %d est %d\n", a, b, c);
+
+  return 0;
+}
