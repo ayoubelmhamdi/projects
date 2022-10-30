a8eebb5352d03ff431d63459dd3a41f52fe1f357
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-10-30 23:25:21 +0100

    init matlab s1

---
+++ src/matlab/trpm-s1/TD1-ex-9-b.m
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int addition(int nomber);
+
+int main() {
+  int N, sum = 0;
+  printf("Donner la valeur de N \n");
+  scanf("%d", &N);
+
+  sum = addition(N);
+  printf("la sum egal %d\n", sum);
+
+  return 0;
+}
+
+int addition(int nomber) {
+  int resultat = 0;
+  for (int i = 0; i <= nomber; i++) {
+    if (i % 2 != 0)
+      resultat = resultat + i;
+  }
+  return resultat;
+}
