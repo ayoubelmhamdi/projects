c33de3fc1c02d3ab3524b9fb244fa1e87cb3e43b
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-10-31 02:14:47 +0100

    up

--- src/matlab/trpm-s1/TD2-ex-4.m
+++ src/matlab/trpm-s1/TD2-ex-4.m
@@ -1,47 +1,50 @@
-#include <stdio.h>
 
-void tri_tableu(int tab[], int n) {
-  for (int i = 0; i < n; i++) {
-    for (int j = i; j < n; j++) {
-      if (tab[i] > tab[j]) {
-        int aide = tab[i];
-        tab[i] = tab[j];
-        tab[j] = aide;
-      }
-    }
-  }
-}
+function tab = tri_tableu(tab, n)
+  for i = 1:n
+    for j = i:n
+      if tab(i) > tab(j)
+        aide = tab(i);
+        tab(i) = tab(j);
+        tab(j) = aide;
+      end
+    end
+  end
+endfunction
 
-int main(int argc, char *argv[]) {
-  int n1 = 6, n2 = 3, n3 = n1 + n2;
-  int tab1[] = {8, 6, 4, 2, 12, 10};
-  int tab2[] = {5, 15, 10};
-  int tab3[n3];
+tab1 = [8, 6, 4, 2, 12, 10];
+tab2 = [5, 15, 10];
+n1 = 6;
+n2 = 3;
+n3 = n1 + n2;
+tab1 = tri_tableu(tab1, n1);
+tab2 = tri_tableu(tab2, n2);
+% disp(tab1)
+% disp(tab2)
 
-  tri_tableu(tab1, 6);
-  tri_tableu(tab2, 3);
-  for (int i = 0; i < n3; i++) {
-    if (i < n1)
-      tab3[i] = tab1[i];
-    else
-      tab3[i] = tab2[i - n1];
-  }
-  tri_tableu(tab3, n3);
+for i = 1:n3
+  if (i <= n1)
+    tab3(i) = tab1(i);
+  else
+    tab3(i) = tab2(i - n1);
+  end
+end
 
-  printf("\naffichage de tab1[]\n");
-  for (int i = 0; i < n1; i++) {
-    printf("%d\t", tab1[i]);
-  }
 
-  printf("\naffichage de tab2[]\n");
-  for (int i = 0; i < n2; i++) {
-    printf("%d\t", tab2[i]);
-  }
+printf("\naffichage de tab1()\n");
+% for i = 1:n1
+%   printf("%d\t", tab1(i));
+% end
+disp(tab1)
 
-  printf("\naffichage de tab3[]\n");
-  for (int i = 0; i < n3; i++) {
-    printf("%d\t", tab3[i]);
-  }
+printf("\naffichage de tab2()\n");
+% for i = 1:n2
+%   printf("%d\t", tab2(i));
+% end
+disp(tab2)
 
-  return 0;
-}
+printf("\naffichage de tab3()\n");
+% for i = 1:n3
+%   printf("%d\t", tab3(i));
+% end
+disp(tab3)
+
